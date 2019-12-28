ccf71399bdc84e6c07e1e36443a5eaea481b496c
Nasty <shekmi1989@gmail.com>
2019-12-28 12:15:36 +0300

    HomeWork_01

---
+++ 01/HomeWork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace DemoApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000); //timeout:TimeSpan.FromDays(5)
+            Console.WriteLine($"Здравствуйте, {name}!");
+            Thread.Sleep(5000);
+            Console.WriteLine($"Прощай, {name}!");
+            Console.ReadKey();
+        }
+    }
+}
