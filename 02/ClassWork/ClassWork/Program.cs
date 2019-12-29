a6ced7bf9fc32258a1a691c052eb1cef45f912c0
yhwh240 <yhwh240>
2019-12-29 14:12:21 +0300

    ClassWork_2

---
+++ 02/ClassWork/ClassWork/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace ClassWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус: ");
+            string radius = Console.ReadLine();
+            radius = radius.Replace(".", ",");
+            double s = 2 * Math.PI * double.Parse(radius);
+            Console.Write("Длина окружности = " + s.ToString());
+
+
+            //Console.WriteLine();
+            //int a = -10;
+            //string myNumberIsText = "4.5";
+            //Console.WriteLine(myNumberIsText);
+            //Console.WriteLine(2 + myNumberIsText + 2);
+            //float number = float.Parse(myNumberIsText, CultureInfo.InvariantCulture);
+            //Console.WriteLine(number);
+
+            Console.ReadLine();
+        }
+    }
+}
