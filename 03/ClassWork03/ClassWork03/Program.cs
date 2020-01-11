50be97af7ab5534a66a04713040108928f567e09
unknown <shekmi1989@gmail.com>
2020-01-11 14:29:45 +0300

    ClassWork

---
+++ 03/ClassWork03/ClassWork03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassWork03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object s = 10;
+            int a = (int)s;
+            a += 5;
+
+            Console.WriteLine(a);
+
+            dynamic d = "string";
+            Console.WriteLine(d.Length);
+            string str = default(string);
+            Console.WriteLine($"{default(string)}");
+
+
+           Console.ReadLine();
+
+        }
+    }
+}
---
+++ 03/ClassWork03_1/ClassWork03_1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Globalization;
+using System.Threading;
+using ClassWork03_1.Properties;
+
+namespace ClassWork03_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			if (args != null && args.Length > 0)
+			{
+				Thread.CurrentThread.CurrentCulture
+					= Thread.CurrentThread.CurrentUICulture
+					= CultureInfo.GetCultureInfo(args[0]);
+			}
+			else
+			{
+				Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+				Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+			}
+
+			Console.InputEncoding = Encoding.ASCII;
+			Console.OutputEncoding = Encoding.Unicode;
+
+			double d;
+			Console.WriteLine(Resources.NumberPrompt);
+			string input = Console.ReadLine();
+			d = double.Parse(input);
+			Console.WriteLine(Resources.Result + d * d);
+
+			//int[] integerArray = new int[3];
+			//integerArray[0] = 1;
+			//integerArray[0] = -5;
+			//integerArray[0] = 999;
+
+			string[] names =
+			{
+				"Emi",
+				"Bob",
+				"Elsa"
+			};
+
+			string[] array = "emi, , , , bob, elsa".Split(
+				separator: ", ",
+				StringSplitOptions.RemoveEmptyEntries);
+			Array.Resize(ref array, newSize: 10); //изменить размер массива
+
+		}
+    }
+}
