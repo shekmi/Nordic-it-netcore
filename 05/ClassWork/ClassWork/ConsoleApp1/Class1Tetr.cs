1266466072d53885abd4ce77846dd9c20e8c68c6
yhwh240 <yhwh240>
2020-01-18 14:19:15 +0300

    ClassWork

---
+++ 05/ClassWork/ClassWork/ConsoleApp1/Class1Tetr.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    enum Color {Red, Green, Blue};
+    class Class1Tetr
+    {
+        public void f()
+        {
+            int value;
+            string output = "";
+            Console.WriteLine("Введите число: ");
+            string input = Console.ReadLine();
+            bool result = int.TryParse(input, out value);
+            if (!result)
+            {
+                output = "Неверный формат";
+            }
+            output = value < 50 ? "Введенное число меньше 50" : "Введенное число больше либо равно 50";
+            Console.WriteLine(output);
+        }
+
+        public void f1()
+        {
+            Color c = (Color)(new Random()).Next(0, 3);
+
+            string output = "";
+            string outputForm = "Договор аренды оформлен на период длительностью ";
+            Console.WriteLine("Введите количество лет договора аренды: ");
+            int years = 0;
+            string input = Console.ReadLine();
+            bool result = int.TryParse(input, out years);
+            if (!result)
+            {
+                output = "Неверный формат";
+            }
+
+            if (years <= 0 || years > 30)
+            {
+                output = "Вы ввели неверное значение!";
+                Console.WriteLine("Вы ввели неверное значение!");
+                return;
+            }
+
+            switch (years)
+            {
+                case 21:
+                case 1:
+                    output = String.Format(outputForm + "{0} год", years);
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                case 22:
+                case 23:
+                case 24:
+                    output = String.Format(outputForm + "{0} годa", years);
+                    break;
+                default:
+                    output = String.Format(outputForm + "{0} лет", years);
+                    break;
+            }
+            Console.WriteLine(output);
+        }
+        public void f2()
+        {
+            int a, b, c;
+            bool isError = false;
+            try
+            {
+                Console.WriteLine("Введите 1 число: ");
+                a = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите 2 число: ");
+                b = int.Parse(Console.ReadLine());
+                c = a / b;
+                Console.WriteLine(c);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(e.Message);
+                Console.ResetColor();
+            }
+            catch (Exception e)
+            {
+                isError = true;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Неизвестная ошибка");
+                Console.ResetColor();
+                throw;
+            }
+            finally //всегда выполняется!!!
+            {
+                if (isError)
+                {
+                    Console.WriteLine("finally block");
+                    Environment.Exit(-1);
+                }
+                Environment.Exit(0);
+            }
+        }
+    }
+
+}
---
+++ 05/ClassWork/ClassWork/ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    [Flags]
+    enum DisplayOption
+    {
+        Binary = 1,
+        Hex = 2
+    };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1Tetr obj = new Class1Tetr();
+            //obj.f();
+            //obj.f1();
+            obj.f2();
+            string output = "";
+            string outputForm = "Договор аренды оформлен на период длительностью ";
+            Console.WriteLine("Введите количество лет договора аренды: ");
+            int years = 0;
+            int yearsOst = 0;
+            string input = Console.ReadLine();
+            bool result = int.TryParse(input, out years);
+            if(!result)
+            {
+                output = "Неверный формат";
+            }
+            
+            if(years < 1 || years > 100)
+            {
+                throw new Exception(message: "Введенное значение выходит за допустимые пределы от до 30!");
+                output = "Вы ввели неверное значение!";
+                Console.WriteLine("Вы ввели неверное значение!");
+                return;
+            }
+
+            //if((years >= 2 && years <= 4) || (years >= 22 && years <= 24))
+            //{
+            //    output = String.Format(outputForm + "{0} годa", years);
+            //}
+            //else if(years == 21 || years == 1)
+            //{
+            //    output = String.Format(outputForm + "{0} год", years);
+            //}
+            //else
+            //{
+            //    output = String.Format(outputForm + "{0} лет", years);
+            //}
+
+            //if (years <= 1 && years >= 10|| years > 30)
+            //{
+
+            if (years > 10 && years < 15)
+            {
+                output = String.Format(outputForm + "{0} лет", years);
+            }
+            else
+            {
+                yearsOst = years % 10;
+                if (yearsOst > 1 && yearsOst < 5)
+                {
+                    output = String.Format(outputForm + "{0} годa", years);
+                }
+                else if (yearsOst == 1)
+                {
+                    output = String.Format(outputForm + "{0} год", years);
+                }
+                else
+                {
+                    output = String.Format(outputForm + "{0} лет", years);
+                }
+            }
+
+
+                Console.WriteLine(output);
+        }
+    }
+}
