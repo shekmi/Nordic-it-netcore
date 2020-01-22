4197b60b3204705683357a945c9c83d0149f95a0
yhwh240 <yhwh240>
2020-01-22 19:36:12 +0300

    ClassWork_06_1

---
+++ 06/ClassWork/ClassWork06/ClassWork06/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassWork06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
