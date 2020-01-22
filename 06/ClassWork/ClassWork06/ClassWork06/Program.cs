882e3772c0411b5a99b71f94f27c613a0c54bdaa
yhwh240 <yhwh240>
2020-01-22 22:09:46 +0300

    ClassWork_06_2

--- 06/ClassWork/ClassWork06/ClassWork06/Program.cs
+++ 06/ClassWork/ClassWork06/ClassWork06/Program.cs
@@ -6,7 +6,67 @@ namespace ClassWork06
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //do
+            //{
+
+
+            //} while (Console.ReadLine() != "Exit");
+
+            //do
+            //{
+            //    string str = Console.ReadLine();
+            //    if (str == "exit")
+            //        continue;
+
+            //} while (Console.ReadLine() != "Exit");
+
+
+
+            //int temp;
+            //do
+            //{
+            //    temp = Console.ReadLine().Length;
+            //    if (temp <= 15)
+            //    {
+            //        Console.WriteLine($"Entered string length is {temp}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Too long string. Try another:");
+            //        continue;
+            //    }
+
+            //    break;
+
+            //} while (true);
+
+            int number;
+            int time = 5;
+            int i = 0;
+            while (i < 2)
+            {
+                Console.WriteLine("Введите число: ");
+                string str = Console.ReadLine();
+
+                bool result = int.TryParse(str, out number);
+                if(!result)
+                {
+                    Console.WriteLine("Не число");
+                    System.Threading.Thread.Sleep(TimeSpan.FromSeconds(time));
+                    time += 10;
+                    i++;
+                }
+                else
+                {                    
+                    Console.WriteLine($"Ваше число в квадрате: {number * number}");
+                }
+            }
         }
+
+ 
+
+       
+
     }
 }
---
+++ 06/ClassWork/ClassWork_for/ClassWork_for/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ClassWork_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double middleValueAll = 0;           
+            double middleValueforWeek = 0;
+            string outputForWeek = "The average mark for day ";
+            string outputForAll = "The average mark for all the week is ";
+            int numDayWeek = 1;
+            int numDayAll = 0;
+            var marks = new[]
+            {
+                new [] {2,3,3,2,3 },
+                new [] { 2, 4, 5, 3},
+                null,
+                new [] { 5, 5, 5, 5},
+                new []{4}
+            };
+
+            for (int i = 0; i < marks.Length; i++)
+            {
+                if (marks[i] == null)
+                {
+                    Console.WriteLine(outputForWeek + $"#{i:0.#} is N/A");
+                    continue;
+                }
+                for (int j = 0; j < marks[i].Length; j++)
+                {
+                    middleValueforWeek += marks[i][j];                    
+                    numDayWeek = j;
+                }
+                middleValueAll += middleValueforWeek;
+                middleValueforWeek = middleValueforWeek / (++numDayWeek);
+
+                numDayAll += numDayWeek;
+                Console.WriteLine(outputForWeek + "#{0} is {1:0.0}", i, middleValueforWeek);
+                numDayWeek = 0;
+                middleValueforWeek = 0;
+            }
+
+            middleValueAll /= numDayAll;
+            Console.WriteLine(outputForAll + "{0:0.0}", middleValueAll);
+        
+        }
+    }
+}
