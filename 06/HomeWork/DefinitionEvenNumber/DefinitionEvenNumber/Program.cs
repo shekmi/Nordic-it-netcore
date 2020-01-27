59fe6c4151650b09edd94d2741dda1a14f71629e
Nasty <shekmi1989@gmail.com>
2020-01-27 21:54:17 +0300

    HomeWork_6_1

---
+++ 06/HomeWork/DefinitionEvenNumber/DefinitionEvenNumber/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace DefinitionEvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            int inputValue = 0;
+            int numEvenNumber = 0;
+            string inputStr = "", outputStr = "";
+            Console.WriteLine("Введите положительное натуральное число не более 2 миллиардов: ");
+            while (true)
+            {
+                try
+                {
+                    inputStr = Console.ReadLine();
+                    inputValue = int.Parse(inputStr);
+                    if (inputValue < 0)
+                    {
+                        outputStr = "Введено неверное значение! Попробуйте ещё раз: ";
+                    }
+                    else if (inputValue == 0)
+                    {
+                        numEvenNumber = 1;
+                        break;
+                    }
+                    else
+                    {
+                        while (inputValue > 0)
+                        {
+                            if (((inputValue % 10) % 2) == 0)
+                            {
+                                numEvenNumber++;
+                            }
+                            inputValue /= 10;
+                        }
+                        break;
+                    }
+                }
+                catch (OverflowException e)
+                {
+                    outputStr = e.Message + " Попробуйте еще раз: ";
+                }
+                catch (FormatException e)
+                {
+                    outputStr = e.Message + " Попробуйте еще раз: ";
+                }
+                Console.WriteLine(outputStr);
+            }
+            Console.WriteLine($"В числе {inputStr} содержится следующее количество четных цифр: {numEvenNumber}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 06/HomeWork/SumCapital/SumCapital/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace DefinitionEvenNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            string output = "";
+            float startContribution;
+            float percent;
+            float wishlullCapital;
+            int numDay = 0;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите сумму первоначального взноса в рублях: ");
+                    startContribution = float.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите ежедневный процент дохода в виде десятичной дроби (1% = 0.01): ");
+                    percent = float.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите желаемую сумму накопления в рублях: ");
+                    wishlullCapital = float.Parse(Console.ReadLine());                   
+                    do
+                    {
+                        startContribution += startContribution * percent;
+                        numDay++;
+                    }
+                    while (wishlullCapital > startContribution);
+                    output = "Необходимое количество дней для накопления желаемой суммы: " + numDay + ".";
+                    break;
+                }
+                catch (OverflowException e)
+                {
+                    output = e.Message + " Попробуйте еще раз! ";
+                }
+                catch (FormatException e)
+                {
+                    output = e.Message + " Попробуйте еще раз! ";
+                }
+                Console.WriteLine(output);               
+            }
+            Console.WriteLine(output);
+            Console.ReadKey();
+        }            
+    }
+}
