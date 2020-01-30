d763a3330463cf3dea474b985d9a981ea8c8557b
shekmi <joke.jokec@yandex.ru>
2020-01-30 22:40:13 +0300

    HomeWork_7_1

---
+++ 07/HomeWork/Polindrom/Polindrom/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace Polindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = "";
+            StringBuilder newOutputString = new StringBuilder();
+            Console.WriteLine("Введите непустую строку:");
+            while (true)
+            {
+                inputString = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(inputString))
+                {
+                    Console.WriteLine("Вы ввели пустую строку :( Попробуйте ещё раз:");
+                    continue;
+                }
+                inputString = inputString.ToLower();
+                for (int i = inputString.Length - 1; i >= 0; i--)
+                {
+                    newOutputString.Append(inputString[i]);
+                }
+                Console.WriteLine(newOutputString);
+                break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 07/HomeWork/ReviewString/ReviewString/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ReviewString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int indexFirstCharacter = 0;
+            string inputStr = "";
+            string[] arrayStr;
+            while (true)
+            {
+                Console.WriteLine("Введите строку из нескольких слов: ");
+                inputStr = Console.ReadLine();                                
+                arrayStr = inputStr.Split(new char[] { ' ', '.', ',', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                if (arrayStr.Length < 2)
+                {
+                    Console.WriteLine("Слишком мало слов :( Попробуйте ещё раз: ");
+                    continue;
+                }
+
+                for (int i = 0; i < arrayStr.Length; i++)
+                {
+                    if (arrayStr[i].StartsWith("А", StringComparison.OrdinalIgnoreCase))
+                    {
+                        indexFirstCharacter++;
+                    }
+                }
+                break;
+            }
+            Console.WriteLine("Количество слов, начинающихся с буквы 'А': " + indexFirstCharacter);
+            Console.ReadKey();
+        }
+    }
+}
