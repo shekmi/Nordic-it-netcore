febb1f2bb55bd296eac13ec3f2e2914641296c01
Nasty <shekmi1989@gmail.com>
2020-02-10 23:56:56 +0300

    HomeWork_10_1

---
+++ 10/HomeWork/DataAboutPeople/DataAboutPeople/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAboutPeople
+{
+    class Person
+    {
+        int _ageNow;
+        public string Name { get; set; }
+        public int AgeNow
+        {
+            get
+            {
+                return _ageNow;
+            }
+            set
+            {
+                if (value > 0 && value < 105)
+                {
+                    _ageNow = value;
+                }
+                else
+                {
+                    throw new FormatException("Invalid value");
+                }
+            }
+        }
+        public int AgeInFourYears { get { return (_ageNow + 4); } }
+
+        public string PrintOutput { get { return $"{Name}, age in 4 years: {AgeInFourYears}"; } }
+    }
+}
+
--- 10/HomeWork/DataAboutPeople/DataAboutPeople/Program.cs
+++ 10/HomeWork/DataAboutPeople/DataAboutPeople/Program.cs
@@ -6,7 +6,33 @@ namespace DataAboutPeople
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = 0;
+            Console.WriteLine("Enter number of people");
+            while(!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("Enter corect value!");
+            }
+            Person[] person = new Person[number];
+            for (int i = 0; i < number; i++)
+            {
+                person[i] = new Person();
+                Console.WriteLine($"Enter name {i}:");
+                person[i].Name = Console.ReadLine();
+                Console.WriteLine($"Enter age {i}:");
+                try
+                {
+                    person[i].AgeNow = int.Parse(Console.ReadLine());
+                }
+                catch(FormatException e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine(person[i].PrintOutput);
+            }
+            Console.ReadKey();
         }
     }
 }
